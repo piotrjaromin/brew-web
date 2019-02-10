9071b640195c0cd857fd30e7956d83d3c6492adc
Piotr Jaromin <piotr.jaromin@performgroup.com>
2019-02-10 14:40:35 +0100

    formatting

--- brew/keg/temperature_control.go
+++ brew/keg/temperature_control.go
@@ -1,87 +1,91 @@
 package keg
 
 import (
-        "log"
-        "time"
+	"log"
+	"time"
 )
 
 type TempControl interface {
-        KeepTemp(temp float64)
-        GetTemp() float64
-        Stop()
-        Start()
+	KeepTemp(temp float64)
+	GetTemp() float64
+	Stop()
+	Start()
 }
 
 type TempControlStruct struct {
-        temp       float64
-        quit       chan struct{}
-        dispresion float64
-        started    bool
-        kegControl KegControl
+	temp       float64
+	quit       chan struct{}
+	dispresion float64
+	started    bool
+	kegControl KegControl
 }
 
 func NewTempControl(kegControl KegControl, temp float64) TempControl {
 
-        log.Println("Creating temp control")
-        tcs := TempControlStruct{
-                temp,
-                make(chan struct{}),
-                1.0,
-                false,
-                kegControl,
-        }
+	log.Println("Creating temp control")
+	tcs := TempControlStruct{
+		temp,
+		make(chan struct{}),
+		1.0,
+		false,
+		kegControl,
+	}
 
-        return TempControl(&tcs)
+	return TempControl(&tcs)
 }
 
 func (tcs *TempControlStruct) KeepTemp(temp float64) {
-        log.Println("new temp to keep is ", temp)
-        if !tcs.started {
-                tcs.Start()
-        }
+	log.Println("new temp to keep is ", temp)
+	if !tcs.started {
+		tcs.Start()
+	}
 
-        tcs.temp = temp
+	tcs.temp = temp
 }
 
 func (tcs TempControlStruct) Stop() {
-        tcs.quit <- struct{}{}
-        tcs.started = false
+	tcs.quit <- struct{}{}
+	tcs.started = false
 }
 
 func (tcs TempControlStruct) Start() {
-        ticker := time.NewTicker(5 * time.Second)
-        tcs.started = true
-        go func() {
-                for {
-                        select {
-                        case <-ticker.C:
+	ticker := time.NewTicker(5 * time.Second)
+	tcs.started = true
+	go func() {
+		go tcs.loopTemp(ticker)
+	}()
+}
 
-                                currTemp, err := tcs.kegControl.Temperature()
-                                if err != nil {
-                                        log.Printf("Error while reading temeprature. %s", err.Error())
-                                        break
-                                }
+func (tcs TempControlStruct) loopTemp(ticker *time.Ticker) {
+	for {
+		select {
+		case <-ticker.C:
 
-                                state := tcs.kegControl.HeaterState(FIRST)
-                                if currTemp + tcs.dispresion > tcs.temp && state {
-                                        log.Println("[tempControl] toggling state of heaters")
-                                        tcs.kegControl.ToggleHeater(FIRST)
-                                        tcs.kegControl.ToggleHeater(SECOND)
-                                }
+			currTemp, err := tcs.kegControl.Temperature()
+			if err != nil {
+				log.Printf("Error while reading temeprature. %s", err.Error())
+				break
+			}
 
-                                if currTemp - tcs.dispresion < tcs.temp && !state {
-                                        tcs.kegControl.ToggleHeater(FIRST)
-                                        tcs.kegControl.ToggleHeater(SECOND)
-                                }
+			state := tcs.kegControl.HeaterState(FIRST)
+			if currTemp+tcs.dispresion > tcs.temp && state {
+				log.Println("[tempControl] toggling state of heaters")
+				tcs.kegControl.ToggleHeater(FIRST)
+				tcs.kegControl.ToggleHeater(SECOND)
+			}
 
-                        case <-tcs.quit:
-                                ticker.Stop()
-                                return
-                        }
-                }
-        }()
+			if currTemp-tcs.dispresion < tcs.temp && !state {
+				tcs.kegControl.ToggleHeater(FIRST)
+				tcs.kegControl.ToggleHeater(SECOND)
+			}
+
+		case <-tcs.quit:
+			ticker.Stop()
+			return
+		}
+	}
 }
 
 func (tcs TempControlStruct) GetTemp() float64 {
-        return tcs.temp
+	return tcs.temp
 }
--- brew/pi/onewire.go
+++ brew/pi/onewire.go
@@ -1,11 +1,11 @@
 package pi
 
 import (
-        "os/exec"
-        "fmt"
-        "io/ioutil"
-        "strings"
-        "strconv"
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 const W1_DEVICES_PATH = "/sys/bus/w1/devices/"
@@ -13,94 +13,94 @@ const W1_SLAVE = "w1_slave"
 const W28_PREFIX = "28"
 
 type W1Device interface {
-        Name() string
-        W1Slave() (string, error)
-        Value(line int, key string) (int, error)
+	Name() string
+	W1Slave() (string, error)
+	Value(line int, key string) (int, error)
 }
 
 type w1Struct struct {
-        name string
-        path string
+	name string
+	path string
 }
 
 func (w1 w1Struct) Name() string {
-        return w1.name
+	return w1.name
 }
 
 func (w1 w1Struct) W1Slave() (string, error) {
-        data, err := ioutil.ReadFile(w1.path + "/" + W1_SLAVE)
-        if err != nil {
-                return "", fmt.Errorf("[W1] Could not read W1Slave data file. Details %+v.", err)
-        }
+	data, err := ioutil.ReadFile(w1.path + "/" + W1_SLAVE)
+	if err != nil {
+		return "", fmt.Errorf("[W1] Could not read W1Slave data file. Details %+v.", err)
+	}
 
-        return string(data), nil
+	return string(data), nil
 }
 
 func (w1 w1Struct) Value(lineNo int, key string) (int, error) {
 
-        v1slave, err := w1.W1Slave()
-        if err != nil {
-                return 0, err
-        }
+	v1slave, err := w1.W1Slave()
+	if err != nil {
+		return 0, err
+	}
 
-        lines := strings.Split(v1slave, "\n")
+	lines := strings.Split(v1slave, "\n")
 
-        if len(lines) < lineNo {
-                return 0, fmt.Errorf("[W1] w1slave does not contains only %d lines(provided %d).", len(lines), lineNo)
-        }
+	if len(lines) < lineNo {
+		return 0, fmt.Errorf("[W1] w1slave does not contains only %d lines(provided %d).", len(lines), lineNo)
+	}
 
-        line := lines[lineNo]
-        keyEntry := key + "="
-        valIndex := strings.Index(line, keyEntry)
-        if valIndex == -1 {
-                return 0, fmt.Errorf("[W1] w1slave line %d does not contain key %s.", lineNo, key)
-        }
-        valIndex += len(keyEntry)
-        return strconv.Atoi(strings.Split(line[valIndex:], " ")[0])
+	line := lines[lineNo]
+	keyEntry := key + "="
+	valIndex := strings.Index(line, keyEntry)
+	if valIndex == -1 {
+		return 0, fmt.Errorf("[W1] w1slave line %d does not contain key %s.", lineNo, key)
+	}
+	valIndex += len(keyEntry)
+	return strconv.Atoi(strings.Split(line[valIndex:], " ")[0])
 }
 
 func Init() error {
 
-        //this one probably require being called with sudo
-        if err := callOs("sudo modprobe w1-gpio'"); err != nil {
-                return err
-        }
+	//this one probably require being called with sudo
+	if err := callOs("sudo modprobe w1-gpio'"); err != nil {
+		return err
+	}
 
-        if err := callOs("sudo modprobe w1-therm"); err != nil {
-                return err
-        }
+	if err := callOs("sudo modprobe w1-therm"); err != nil {
+		return err
+	}
 
-        return nil
+	return nil
 }
 
 func GetDevices() ([]W1Device, error) {
-        devices := make([]W1Device, 0, 1)
+	devices := make([]W1Device, 0, 1)
 
-        dirs, err := ioutil.ReadDir(W1_DEVICES_PATH)
-        if err != nil {
-                return devices, fmt.Errorf("[W1] could not read directory %s. Details %+v", W1_DEVICES_PATH, err)
-        }
+	dirs, err := ioutil.ReadDir(W1_DEVICES_PATH)
+	if err != nil {
+		return devices, fmt.Errorf("[W1] could not read directory %s. Details %+v", W1_DEVICES_PATH, err)
+	}
 
-        for _, w1dir := range dirs {
+	for _, w1dir := range dirs {
 
-                if (strings.HasPrefix(w1dir.Name(), W28_PREFIX)) {
-                        devices = append(devices, w1Struct{
-                                name: w1dir.Name(),
-                                path: W1_DEVICES_PATH + "/" + w1dir.Name(),
-                        })
-                }
-        }
+		if strings.HasPrefix(w1dir.Name(), W28_PREFIX) {
+			devices = append(devices, w1Struct{
+				name: w1dir.Name(),
+				path: W1_DEVICES_PATH + "/" + w1dir.Name(),
+			})
+		}
+	}
 
-        return devices, nil
+	return devices, nil
 }
 
 func callOs(command string) error {
 
-        cmd := exec.Command(command)
-        err := cmd.Run()
-        if err != nil {
-                return fmt.Errorf("[W1] Could not init 1-wire for command %s. Details: %+v", command, err)
-        }
+	cmd := exec.Command(command)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("[W1] Could not init 1-wire for command %s. Details: %+v", command, err)
+	}
 
-        return nil
+	return nil
 }
